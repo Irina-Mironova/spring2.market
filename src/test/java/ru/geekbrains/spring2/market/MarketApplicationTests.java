bb6b7d3440610d955b2b0481aa1ac00dc6ba74ea
Ирина <irina250282@mail.ru>
2022-02-14 10:08:10 +0500

    Init

---
+++ src/test/java/ru/geekbrains/spring2/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.spring2.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
